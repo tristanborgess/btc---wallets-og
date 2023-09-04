5ed60559dbcb02e95fa8474ee79ac38933e49238
Tristan Borges Solari <tristanborgessolari@Tristans-MacBook-Pro.local>
2023-09-04 18:28:36 -0400

    fixed cors

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const PORT = 3000;
 
 // CORS headers
     app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
         res.header(
             'Access-Control-Allow-Methods', 
             'OPTIONS, HEAD, GET, PUT, POST, DELETE'
