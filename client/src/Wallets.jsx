d06925b8c38b069bd21d061a2092d1f7284df071
Tristan Borges Solari <tristanborgessolari@Tristans-MacBook-Pro.local>
2023-09-06 11:59:16 -0400

    added filter and loading state

--- client/src/Wallets.jsx
+++ client/src/Wallets.jsx
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Tabs from './Tabs';
 import SearchWallets from './SearchWallets';
+import WalletFilter from './WalletFilter';
 
 const Wallets = () => {
 const [wallets, setWallets] = useState([]);
+const [loading, setLoading] = useState(false);
 const features = wallets.length > 0 ? Object.keys(wallets[0]).filter(feature => feature !== '_id' && feature !== 'Category') : [];
 const [activeCategory, setActiveCategory] = useState('On-chain');
 
 const [displayAllWallets, setDisplayAllWallets] = useState(true);
-const [hasSearched, setHasSearched] = useState(false);
 const [searchedWallets, setSearchedWallets] = useState([]);
+
+const [shownColumns, setShownColumns] = useState(['Name']);
 
 //For the wallet search feature. It checks if the wallet is already displayed and adds the searched wallet to the displayWallets array
 const handleSearch = (wallet) => {
@@ -31,15 +34,25 @@ const handleClearSearch = () => {
 
 const displayWallets = displayAllWallets ? wallets : searchedWallets;
 
-console.log("Rendering with displayWallets:", displayWallets);
+const handleColumnToggle = (newShownColumns) => {
+    setShownColumns(newShownColumns);
+};
 
 useEffect(() => {
+    setLoading(true);
     fetch(`http://localhost:3000/wallets/${activeCategory}`)
         .then(response => response.json())
         .then(data => {
             setWallets(data.data);
+            const newFeatures = data.data.length > 0 ? Object.keys(data.data[0]).filter(feature => feature !== '_id' && feature !== 'Category') : [];
+            const columnsToAdd = newFeatures.filter(feature => !shownColumns.includes(feature));
+            setShownColumns(prev => ['Name', ...columnsToAdd.slice(0, 6)]);
+            setLoading(false);
         })
-        .catch(err => console.error(err));
+        .catch(err => {
+            console.error(err);
+            setLoading(false);
+        });
 }, [activeCategory]);
 
 return (
@@ -55,10 +68,14 @@ return (
                 wallets={wallets}
                 hasSearched={!displayAllWallets}
             />
+            <WalletFilter columns={features} shownColumns={shownColumns} onColumnToggle={handleColumnToggle} />
+            {loading ? (
+                <LoadingMessage>Loading wallets...</LoadingMessage>
+            ) : (
             <StyledTable>
                 <thead>
                     <tr>
-                        {features.map(feature => (
+                        {shownColumns.map(feature => (
                         <StyledTableHeader key={feature}>{feature}</StyledTableHeader>
                         ))}
                     </tr>
@@ -66,7 +83,7 @@ return (
                 <tbody>
                     {displayWallets.map(wallet => (
                         <tr key={wallet._id}>
-                            {features.map(feature => (
+                            {shownColumns.map(feature => (
                                 <StyledTableCell key={feature}>
                                     {typeof wallet[feature] === "object" ? JSON.stringify(wallet[feature]) : wallet[feature]}
                                 </StyledTableCell>
@@ -75,6 +92,7 @@ return (
                 ))}
                 </tbody>
             </StyledTable>
+            )}
         </>
     );  
 }
@@ -96,6 +114,13 @@ return (
     padding: 8px 12px;
     background-color: #f2f2f2;
     text-align: left;
+    `;
+
+    const LoadingMessage = styled.div`
+    text-align: center;
+    padding: 20px;
+    font-size: 20px;
+    font-weight: bold;
     `;
 
 export default Wallets;
